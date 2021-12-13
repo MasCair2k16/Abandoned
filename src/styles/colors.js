149131f55703d7cccda8af363a46754ac5cc990c
MasCair2k16 <gotgame6@msn.com>
2021-12-12 17:33:04 -0800

    feature: add style structure

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+// BASICS
+export const PRIMARY = '#435058';
+export const SECONDARY = '#E4572E';
+export const ASSCENT = '#FCFC62';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+
+// ACTIONS
+export const SUCCESS = '#3adb76';
+export const WARNING = '#ffae00';
+export const ALERT = '#cc4b37';
+
+// GRAYSCALE
+export const GRAY_LIGHT = '#e6e6e6';
+export const GRAY_MEDIUM = '#cacaca';
+export const GRAY_DARK = '#8a8a8a';
